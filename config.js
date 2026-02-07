78c15c740138923b4163cd7b09bb5178059fe082
Bloxco12 <118644302+Bloxco12@users.noreply.github.com>
2026-02-07 16:07:55 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
     client: {
-        token: '', // ← Your bot token (.env IS RECOMMENDED)
-        id: '' // ← Your bot ID
+        token: 'process.env.DISCORD_TOKEN', // ← Your bot token (.env IS RECOMMENDED)
+        id: 'process.env.CLIENT_ID' // ← Your bot ID
     },
     modmail: {
-        guildId: '', // ← Your server ID
-        categoryId: '', // ← The modmail category ID
-        staffRoles: [''], // ← The modmail staff roles IDs
+        guildId: '1461381254229262408', // ← Your server ID
+        categoryId: '1461825003539402954', // ← The modmail category ID
+        staffRoles: ['1463930730332819477'], // ← The modmail staff roles IDs
         mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
     },
     logs: {
-        webhookURL: '' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
+        webhookURL: 'process.env.WEBHOOK' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
     }
 };
